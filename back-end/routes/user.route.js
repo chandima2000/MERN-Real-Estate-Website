8f98d020fffc0d0d698fd784c7c368b07f8d8663
chandima2000 <hychandima2000@gmail.com>
2024-02-29 21:07:06 +0530

    Create get user listings API route

--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'; //Express is used to create the routes
-import { deleteUser, test,updateUser } from '../controllers/user.controller.js';
+import { deleteUser,getUserListings, test,updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/test',test) ;
 router.post('/update/:id',verifyToken,updateUser);
-router.delete('/delete/:id',verifyToken,deleteUser)
+router.delete('/delete/:id',verifyToken,deleteUser);
+router.get('/listings/:id',verifyToken,getUserListings);
 
 export default router; 
