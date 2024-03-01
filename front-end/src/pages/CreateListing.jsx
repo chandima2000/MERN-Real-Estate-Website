6c9361e352c9988259eacd3e1486733c67118f5f
chandima2000 <hychandima2000@gmail.com>
2024-03-01 15:39:23 +0530

    Complete the listing page

--- front-end/src/pages/CreateListing.jsx
+++ front-end/src/pages/CreateListing.jsx
@@ -295,7 +295,9 @@ const handleSubmit = async (e) => {
                         />
                         <div className="flex flex-col items-center">
                             <p>Regular price</p>
-                            <span className='text-xs'>($ / month)</span>
+                            {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
                         </div>
                     </div>
 
@@ -313,7 +315,9 @@ const handleSubmit = async (e) => {
                             />
                             <div className="flex flex-col items-center">
                                 <p>Discounted price</p>
-                                <span className='text-xs'>($ / month)</span>
+                                {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                             </div>
                         </div>
                     )}
--- front-end/src/pages/UpdateListing.jsx
+++ front-end/src/pages/UpdateListing.jsx
@@ -314,7 +314,9 @@ const handleSubmit = async (e) => {
                         />
                         <div className="flex flex-col items-center">
                             <p>Regular price</p>
-                            <span className='text-xs'>($ / month)</span>
+                            {formData.type === 'rent' && (
+                        <span className='text-xs'>($ / month)</span>
+                        )}
                         </div>
                     </div>
 
@@ -332,7 +334,9 @@ const handleSubmit = async (e) => {
                             />
                             <div className="flex flex-col items-center">
                                 <p>Discounted price</p>
-                                <span className='text-xs'>($ / month)</span>
+                                {formData.type === 'rent' && (
+                          <span className='text-xs'>($ / month)</span>
+                          )}
                             </div>
                         </div>
                     )}
