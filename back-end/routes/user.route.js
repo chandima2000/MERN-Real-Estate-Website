2b52d63cc3c54b78904d990c6d949cc80aeae368
chandima2000 <hychandima2000@gmail.com>
2024-03-01 17:23:36 +0530

    Add contact landLoad functionality to the listing page

--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'; //Express is used to create the routes
-import { deleteUser,getUserListings, test,updateUser } from '../controllers/user.controller.js';
+import { deleteUser,getUserListings, test,updateUser,getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,5 +9,6 @@ router.get('/test',test) ;
 router.post('/update/:id',verifyToken,updateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
 router.get('/listings/:id',verifyToken,getUserListings);
+router.get('/:id',verifyToken,getUser)
 
 export default router; 
